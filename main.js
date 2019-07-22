ca4b9bebc576f22c4076f97a7e52f6ad1b7370b0
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-22 19:57:52 +0200

    ok

--- main.js
+++ main.js
@@ -5,24 +5,80 @@ $( document ).ready(function() {
      function (){
         var inputval = $("#valmsg").val();
 
-        var inviato = $("#template .msg-inviato").clone();
-        var figlio = inviato.children(".testo-messaggio");
+        //per scrivere l orario
+          function addZero(i) {
+            if (i < 10) {
+              i = "0" + i;
+            }
+            return i;
+          }
 
-        figlio.text(inputval);
-        $("#lista-messaggi").append(inviato);
+          var d = new Date();
+          var h = addZero(d.getHours());
+          var m = addZero(d.getMinutes());
 
-        //pulisco l'input alla fine
-        $("#valmsg").val("");
+          //clono il figlio del contenitore
+          var inviato = $("#template .msg-inviato").clone();
+          var figlio = inviato.children(".testo-messaggio");
+
+          var ore = inviato.children(".orario");
+          ore.text(h + " " +  m);
+
+          //scrivo il messaggio preso da input
+          figlio.text(inputval);
+          //stampo il clone con l input e l orario
+          $("#lista-messaggi").append(inviato);
+
+          //pulisco l'input alla fine
+          $("#valmsg").val("");
 
 
-        // var risp = $("#template .msg-risp").clone();
-        // $("#lista-messaggi").append(risp);
+          var risp = $("#template .msg-risp").clone();
+          $("#lista-messaggi").append(risp);
 
      }
   );
 
 
 
+  $('#valmsg').keypress(function(e){
+      if(e.keyCode == 13)
+      {
+        var inputval = $("#valmsg").val();
+
+        //per scrivere l orario
+          function addZero(i) {
+            if (i < 10) {
+              i = "0" + i;
+            }
+            return i;
+          }
+
+          var d = new Date();
+          var h = addZero(d.getHours());
+          var m = addZero(d.getMinutes());
+
+          //clono il figlio del contenitore
+          var inviato = $("#template .msg-inviato").clone();
+          var figlio = inviato.children(".testo-messaggio");
+
+          var ore = inviato.children(".orario");
+          ore.text(h + " " +  m);
+
+          //scrivo il messaggio preso da input
+          figlio.text(inputval);
+          //stampo il clone con l input e l orario
+          $("#lista-messaggi").append(inviato);
+
+          //pulisco l'input alla fine
+          $("#valmsg").val("");
+
+
+          var risp = $("#template .msg-risp").clone();
+          $("#lista-messaggi").append(risp);
+
+      }
+  });
 
 
 
