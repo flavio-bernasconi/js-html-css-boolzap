fe83cf85a719762c2fbbf3dade68d4181efb7e05
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-23 17:31:47 +0200

    ok

--- main.js
+++ main.js
@@ -1,103 +1,54 @@
 $( document ).ready(function() {
 
+  function invio(){
+     var inputval = $("#valmsg").val();
+
+     if (inputval != "") {
+       //per scrivere l orario
+         function addZero(i) {
+           if (i < 10) {
+             i = "0" + i;
+           }
+           return i;
+         }
+
+         var d = new Date();
+         var h = addZero(d.getHours());
+         var m = addZero(d.getMinutes());
+
+         //clono il figlio del contenitore
+         var inviato = $("#template .msg-inviato").clone();
+         var figlio = inviato.children(".testo-messaggio");
+
+         var ore = inviato.children(".orario");
+         ore.text(h + " " +  m);
+
+         //scrivo il messaggio preso da input
+         figlio.text(inputval);
+         //stampo il clone con l input e l orario
+         $("#lista-messaggi").append(inviato);
+
+         //pulisco l'input alla fine
+         $("#valmsg").val("");
+
+         setTimeout(function(){
+           var risp = $("#template .msg-risp").clone();
+           $("#lista-messaggi").append(risp);
+         },1000);
+     }
+     else {
+       console.log("vuotoooo");
+     }
+  }
 
   $("#send").click(
-     function (){
-        var inputval = $("#valmsg").val();
-
-        if (inputval != "") {
-          //per scrivere l orario
-            function addZero(i) {
-              if (i < 10) {
-                i = "0" + i;
-              }
-              return i;
-            }
-
-            var d = new Date();
-            var h = addZero(d.getHours());
-            var m = addZero(d.getMinutes());
-
-            //clono il figlio del contenitore
-            var inviato = $("#template .msg-inviato").clone();
-            var figlio = inviato.children(".testo-messaggio");
-
-            var ore = inviato.children(".orario");
-            ore.text(h + " " +  m);
-
-            //scrivo il messaggio preso da input
-            figlio.text(inputval);
-            //stampo il clone con l input e l orario
-            $("#lista-messaggi").append(inviato);
-
-            //pulisco l'input alla fine
-            $("#valmsg").val("");
-
-            setTimeout(function(){
-              var risp = $("#template .msg-risp").clone();
-              $("#lista-messaggi").append(risp);
-            },1000);
-        }
-        else {
-          console.log("vuotoooo");
-        }
-
-
-
-
-
-
-
-
-     }
+    invio
   );
-
-
 
   $('#valmsg').keypress(function(e){
       if(e.keyCode == 13)
       {
-        var inputval = $("#valmsg").val();
-
-        var inputval = $("#valmsg").val();
-
-        if (inputval != "") {
-          //per scrivere l orario
-            function addZero(i) {
-              if (i < 10) {
-                i = "0" + i;
-              }
-              return i;
-            }
-
-            var d = new Date();
-            var h = addZero(d.getHours());
-            var m = addZero(d.getMinutes());
-
-            //clono il figlio del contenitore
-            var inviato = $("#template .msg-inviato").clone();
-            var figlio = inviato.children(".testo-messaggio");
-
-            var ore = inviato.children(".orario");
-            ore.text(h + " " +  m);
-
-            //scrivo il messaggio preso da input
-            figlio.text(inputval);
-            //stampo il clone con l input e l orario
-            $("#lista-messaggi").append(inviato);
-
-            //pulisco l'input alla fine
-            $("#valmsg").val("");
-
-            setTimeout(function(){
-              var risp = $("#template .msg-risp").clone();
-              $("#lista-messaggi").append(risp);
-            },1000);
-        }
-        else {
-          console.log("vuotoooo");
-        }
-
+        invio();
       }
   });
 
