93196928e2914867adf8b07a05b798e946aae569
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-24 12:44:35 +0200

    mm nah non funziona il cambio utente

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ $( document ).ready(function() {
      }
   }
 
+  //ruchiamo le funzioni sia qando clicco che con invio
   $("#send").click(
     invio
   );
@@ -56,67 +57,61 @@ $( document ).ready(function() {
 
 
 
-var arr = [];
 var stringa = "";
 var nome;
 var maiu = "";
 var contatto = '';
-var splttata = '';
 var cerca = $('#search').val();
 
 if (cerca === "") {
   console.log("valore del cerca non ceee");
-
 }
 
   $('#search').keydown(function(e){
 
     var tasto = e.which;
+
     // da 65 a 90
-    console.log(tasto);
-       if(
-               !(tasto >= 65 && tasto <= 120 || tasto == 8) && (tasto != 32 && tasto != 0)){
-                e.preventDefault();
-       }
+    console.log("tasto prpemuto",tasto);
+       if(!(tasto >= 65 && tasto <= 120 || tasto == 8) && (tasto != 32 && tasto != 0)){
+         //non scrivere nell input
+          e.preventDefault();
+       } else {
+          //converto il tasto nella corrispndente lettera
+           var keyLettera = String.fromCharCode(tasto);
+           //concateno le lettere per creare la stringa da cercare
+           stringa = stringa + keyLettera;
+           console.log("valore stringa",stringa);
+           //se il tasto é canc togli ultimi 2 valori alla stringa
+           // 2 valore perché l ultimo carattere sara il canc stesso che ho appena premuto
+           //se infatti sottraggo 1 non funziona
+           if (tasto == 8) {
+             stringa = stringa.substring(0, stringa.length - 2);
+             console.log("nuovo valore",stringa);
+           }
 
-       else {
-                //converto il tasto nella corrispndente lettera
-               var res = String.fromCharCode(tasto);
-               //concateno le lettere per creare la stringa da cercare
-               stringa = stringa + res;
-               console.log("valore stringa",stringa);
 
-               //se il tasto é canc togli al valore di stringa 2 valori
-               // 2 valore perché l ultimo carattere sara il canc stesso che ho appena premuto
-               //se infatti sottraggo 1 non funziona perche non toglia l ultima lettera
-               if (tasto == 8) {
-                 stringa = stringa.substring(0, stringa.length - 2);
-                 console.log("nuovo valore",stringa);
+           var nomeContatto;
+           var contatto = $(".utente");
+           //appena schiaccio qualcosa nell input nascondo tutti gli utenti
+           contatto.hide(100);
+
+
+           contatto.each(function(i) {
+              //di ogni contatto trovo l elemento con la classe nome
+               var nomeContatto = $(this).find(".nome").text();
+               // console.log(nomeContatto);
+
+               //testo contenuto dalla classe nome
+               nome = nomeContatto.toUpperCase();
+               // console.log("stampo nome",nome);
+
+               //se il testo contiene i valori di stringa mostrami l utente che ha quel nome
+               if (nome.includes(stringa)) {
+                 // console.log("stampo utente trovato ",this);
+                 $(this).fadeIn(100);
                }
-
-
-               var nomeContatto;
-               var contatto = $(".utente");
-               //appena schiaccio qualcosa nell input nascondo tutti gli utenti
-               contatto.hide(100);
-
-               contatto.each(function(i) {
-                  //di ogni contatto trovo l elemento con la classe nome
-                   var nomeContatto = $(this).find(".nome");
-                   // console.log(nomeContatto);
-
-                   //testo contenuto dalla classe nome
-                   maiu = $( nomeContatto ).text();
-                   nome = maiu.toUpperCase();
-                   // console.log("stampo nome",nome);
-
-                   //se il testo contiene i valori di stringa mostrami l utente che ha quel nome
-                   if (nome.includes(stringa)) {
-                     // console.log("stampo utente trovato ",this);
-                     $(this).fadeIn(100);
-
-                   }
-                 });
+             });
 
 
        }
@@ -130,7 +125,27 @@ if (cerca === "") {
 
 
 
+    $(".utente").click(
+      function(){
+        var nomeContatto = $(this).find(".nome").text().toLowerCase();
+        console.log(nomeContatto);
+        $(".utente").removeClass("active");
+        $(this).addClass("active");
+        //questo utente ha il nome della classe usa this
+        $(".contenitore").removeClass("active");
 
+        var conte = $(".contenitore").hasClass("nomeContatto");
+        console.log(conte);
+        if (conte) {
+          $(".contenitore.nomeContatto").addClass("active");
+        }
+        else {
+          console.log("non va");
+        }
+
+
+      }
+    )
 
 
 
